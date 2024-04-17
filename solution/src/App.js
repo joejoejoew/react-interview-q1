c770f907c3579a163ee9d2eecb29b00242cf314b
Joe Wright <joejoejoew@gmail.com>
2024-04-17 12:05:23 -0700

    intial working solution

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { getLocations, isNameValid } from './mock-api/apis';
 
 function App() {
+  const [name, setName] = useState('');
+  const [nameIsValid, setNameIsValid] = useState(undefined);
+  const [locations, setLocations] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState('');
+  const [submissions, setSubmissions] = useState([]);
+
+  useEffect(() => {
+    getLocations().then((resp) => {
+      setLocations(resp);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form>
+        <div>
+          <label>
+            Name
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => {
+                setNameIsValid(undefined);
+                isNameValid(e.target.value).then((resp) => {
+                  setNameIsValid(resp);
+                });
+                setName(e.target.value);
+              }}
+            />
+            <span className="error-message" style={{ visibility: nameIsValid !== false ? 'hidden' : 'visible' }}>this name has already been taken</span>
+          </label>
+        </div>
+        <div>
+          <label>
+            Country
+            <select
+              value={selectedLocation}
+              onChange={(e) => setSelectedLocation(e.target.value)}
+            >
+              <option key="" value="">
+                Select a country
+              </option>
+              {locations.map((country) => (
+                <option key={country} value={country}>
+                  {country}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
+        <button
+          type="button"
+          disabled={submissions?.length === 0}
+          onClick={() => setSubmissions([])}
         >
-          Learn React
-        </a>
-      </header>
+          Clear
+        </button>
+        <button
+          type="button"
+          disabled={!nameIsValid || name === "" || selectedLocation === ""}
+          onClick={() => {
+            setSubmissions((prev) => [
+              ...prev,
+              {
+                name,
+                country: selectedLocation,
+              },
+            ]);
+          }}
+        >
+          Add
+        </button>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Country</th>
+            </tr>
+          </thead>
+          <tbody>
+            {submissions?.map((submission, index) => (
+              <tr key={index}>
+                <td>{submission.name}</td>
+                <td>{submission.country}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </form>
     </div>
   );
 }
